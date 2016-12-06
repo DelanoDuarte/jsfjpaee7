f1476cd307f40d2b6ded39252d995cb7d248b9fa
Delano <delano.duarte@gmail.com>
2016-12-06 19:06:09 -0300

    Removendo diretoria duplicado do projeto !

--- jsfjpaee7-master/src/br/com/simpleapp/util/RestActivator.java
+++
@@ -1,9 +0,0 @@
-package br.com.simpleapp.util;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("/rest/api")
-public class RestActivator extends Application {
-
-}
