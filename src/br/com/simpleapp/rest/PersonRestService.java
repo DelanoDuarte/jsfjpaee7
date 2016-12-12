3df2607840a7869654c62474783fe7bf5edacf36
Delano <delano.duarte@gmail.com>
2016-12-11 21:18:44 -0300

    Configurando Rest API com CDI, apresentando problemas de integração com o CDI !

--- src/br/com/simpleapp/rest/PersonRestService.java
+++ src/br/com/simpleapp/rest/PersonRestService.java
@@ -6,7 +6,6 @@ package br.com.simpleapp.rest;
 import java.io.Serializable;
 import java.util.List;
 
-import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
@@ -15,6 +14,7 @@ import javax.ws.rs.core.MediaType;
 
 import br.com.simpleapp.domain.Person;
 import br.com.simpleapp.repository.PersonRepository;
+import br.com.simpleapp.service.PersonService;
 
 /**
  * @author delan
@@ -28,15 +28,16 @@ public class PersonRestService implements Serializable {
 	 */
 	private static final long serialVersionUID = 1L;
 
-	@Inject
-	private PersonRepository personRepository;
+	private PersonRepository personRepository = new PersonRepository();
+
+	private PersonService personService = new PersonService();
 
 	@Path("/list")
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
 	public List<Person> todosFuncionáriosJSON() {
 		try {
-			return personRepository.findAll();
+			return personRepository.buscarTodos();
 		} catch (Exception e) {
 			return null;
 		}
@@ -51,6 +52,37 @@ public class PersonRestService implements Serializable {
 		} catch (Exception e) {
 			return null;
 		}
+	}
+
+	@GET
+	@Path("/calculo13Salario/{id}/{meses}")
+	public double calcular13SalarioFuncionario(@PathParam(value = "id") Long id,
+			@PathParam(value = "meses") Integer meses) {
+		try {
+			Person person = personRepository.findById(id);
+			double resultado = personService.calcular13Salario(person, meses);
+			return resultado;
+		} catch (Exception e) {
+			System.out.println("Erro: " + e.getMessage());
+			return 0.0;
+		}
+
+	}
+
+	public PersonRepository getPersonRepository() {
+		return personRepository;
+	}
+
+	public void setPersonRepository(PersonRepository personRepository) {
+		this.personRepository = personRepository;
+	}
+
+	public PersonService getPersonService() {
+		return personService;
+	}
+
+	public void setPersonService(PersonService personService) {
+		this.personService = personService;
 	}
 
 }
